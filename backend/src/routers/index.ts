e06afe9edf041dd8b6ec7b0c9b137f292200fd09
ujwal-devpX <sujwals2005@gmail.com>
2026-08-02 21:27:55 +0530

    updated routers

--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -3,6 +3,7 @@ import healthRouter from "./v1/health.routers.js";
 import authRouter from "./v1/auth.routers.js";
 import applicationRouter from "./v1/application.routers.js"
 import apiKeyRouter from "./v1/api-key.routers.js";
+import ingestRouter from "./v1/ingestion.routes.js"
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/v1/health", healthRouter);
 router.use("/v1/auth", authRouter);
 router.use("/v1/applications", applicationRouter);
 router.use("/v1/applications/:applicationId/api-keys", apiKeyRouter);
+router.use("/v1/ingest", ingestRouter)
 
 export default router;
